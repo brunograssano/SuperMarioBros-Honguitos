cd3cc10c2fa574a2e9ced07f46fb2f1d53cdc007
adrianromero99 <adrianromero99@htomail.com>
2020-10-10 10:29:39 -0300

    Carga de Mario en pantalla
    Me parece que me olvide de hacer un delete Mario (agregar)
    
    Co-Authored-By: Bruno Grassano <53836177+brunograssano@users.noreply.github.com>
    Co-Authored-By: Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
+//Using SDL, SDL_image, standard IO, and strings
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
 #include <stdio.h>
-#include <iostream>
 #include <string>
-using namespace std;
 #include "modelo/Mario.h"
 #include "modelo/Movimientos/Movimiento.h"
 
@@ -12,7 +10,10 @@ using namespace std;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-//Starts up SDL and creates window
+
+
+
+//Starts up SDL
 bool init();
 
 //Loads media
@@ -24,11 +25,11 @@ void close();
 //Loads individual image
 SDL_Surface* loadSurface( std::string path );
 
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
 
 //The surface contained by the window
 SDL_Surface* gScreenSurface = NULL;
@@ -37,62 +38,58 @@ SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gPNGSurface = NULL;
 
 bool init(){
+
 	//Initialization flag
 	bool success = true;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
+	//Inicializa SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
 		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
+		return !success;
 	}
-	else
-	{
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Initialize PNG loading
-			int imgFlags = IMG_INIT_PNG;
-			if( !( IMG_Init( imgFlags ) & imgFlags ) )
-			{
-				printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Get window surface
-				gScreenSurface = SDL_GetWindowSurface( gWindow );
-			}
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}else{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-			}
-		}
+
+	//Crea una ventana
+	gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+	if( gWindow == NULL ){
+		printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+		return !success;
 	}
+
+	//Initializo la biblioteca SDL_Image
+	int imgFlags = IMG_INIT_PNG;
+
+	if( !( IMG_Init( imgFlags ) & imgFlags ) ){
+		printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+		return !success;
+	}
+
+	gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+	if( gRenderer == NULL ){
+		printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+		return !success;
+	}
+
+	//Initialize renderer color
+	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+
+
+	//Obtengo la (Surface) superficie de la ventana
+	gScreenSurface = SDL_GetWindowSurface( gWindow );
 
 	return success;
 }
 
-bool loadMedia(){
+bool loadMedia()
+{
 	//Loading success flag
 	bool success = true;
 
-
-	//Load PNG surface
+	//Cargo la imagen png
 	gPNGSurface = loadSurface( "./resources/comienzo.png" );
-	if( gPNGSurface == NULL )
-	{
+
+	if( gPNGSurface == NULL ){
 		printf( "Failed to load PNG image!\n" );
 		success = false;
 	}
@@ -100,59 +97,63 @@ bool loadMedia(){
 	return success;
 }
 
-void close(){
-	//Free loaded image
-	SDL_FreeSurface(gPNGSurface);
+void close()
+{
+	//Libero la imagen cargada
+	SDL_FreeSurface( gPNGSurface );
 	gPNGSurface = NULL;
 
-	//Destroy window
+	//Destruyo el renderer
 	SDL_DestroyRenderer( gRenderer );
+
+	//Destruyo la ventanta
 	SDL_DestroyWindow( gWindow );
 	gWindow = NULL;
 
-	//Quit SDL subsystems
+	//Quiteo los subsistemas de SDL
 	IMG_Quit();
 	SDL_Quit();
 }
 
-SDL_Surface* loadSurface(std::string path){
+SDL_Surface* loadSurface( std::string path )
+{
 	//The final optimized image
-
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-	}
 	SDL_Surface* optimizedSurface = NULL;
-	{
-		//Convert surface to screen format
+
+	//Cargo la imagen que se encuentra en el path especificado
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL ){
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}else{
+		//Convierto la Surface al formato del Screen
 		optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
-		if(optimizedSurface == NULL){
+		if( optimizedSurface == NULL ){
 			printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
 		}
 
-		//Get rid of old loaded surface
+		//Me deshago de la Surface vieja
 		SDL_FreeSurface( loadedSurface );
 	}
 
-	return loadedSurface;
+	return optimizedSurface;
 }
 
-int main(int argc, char* args[]){
-	//Start up SDL and create window
-	if(!init()){
-		printf( "Failed to initialize!\n" );
-		return -1;
-	}
-	//Load media
-	if(!loadMedia()){
-		cout << "Failed to load media!\n" << endl;
+int main( int argc, char* args[] )
+{
+	//Inicializa SDL y crea una ventana
+	if( !init() ){
+		printf( "Ha fallado la inicializacion de SDL\n" );
 		close();
 		return -1;
 	}
-	cout << "strike 2" << endl;
-	//Create renderer for window
+
+	//Load media, se carga una imagen inicial en la ventana
+	if( !loadMedia() ){
+		printf( "Ha fallado la carga de imagenes incial" );
+		close();
+		return -1;
+	}
+
 	//Main loop flag
 	bool quit = false;
 	bool comenzar = false;
@@ -160,24 +161,30 @@ int main(int argc, char* args[]){
 	SDL_Event e;
 
 	//While application is running
-	while(!quit || !comenzar){
+	while(!quit && !comenzar){
+
+		//Aplicar la imagen PNG
+		SDL_BlitSurface( gPNGSurface, NULL, gScreenSurface, NULL );
+
+		//Actualizar surface
+		SDL_UpdateWindowSurface( gWindow );
+
 		//Handle events on queue
 		while(SDL_PollEvent(&e) != 0){
 			//User requests quit
 			if(e.type == SDL_QUIT){
 				quit = true;
-			}
-			else if(e.key.keysym.sym  == SDLK_RETURN){
+				printf("Se ha cerrado la aplicacion");
+			}else if(e.key.keysym.sym  == SDLK_RETURN){
 				comenzar = true;
+				printf("Se ha comenzado la aplicacion");
 			}
 		}
 
-		//Apply the PNG image
-		SDL_BlitSurface(gPNGSurface, NULL, gScreenSurface, NULL);
-		//Update the surface
-		SDL_UpdateWindowSurface(gWindow);
-		cout << "strike 2.5" << endl;
 	}
+
+	// Si se comnezo: comenzar == true; si se quiteo: quit == true
+
 
 	Mario* mario = new Mario();
 	gPNGSurface = loadSurface( "./resources/marioElduro.png" );
@@ -230,8 +237,8 @@ int main(int argc, char* args[]){
 		//Update the surface
 		SDL_UpdateWindowSurface(gWindow);
 	}
-	delete mario;
-	//Free resources and close SDL
+
+	//Liberar recursos y cerrar SDL
 	close();
 
 	return 0;

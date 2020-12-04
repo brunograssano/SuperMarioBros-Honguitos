3964fc1426c00ebdf7e584ae479d827ff175cd93
Joaquin Gomez <joaqogomez@gmail.com>
2020-12-04 19:25:36 -0300

    primer approach a placeholders

--- src/Client/app/VentanaInicio/BotonConTexto.cpp
+++ src/Client/app/VentanaInicio/BotonConTexto.cpp
@@ -1,13 +1,46 @@
 #include "BotonConTexto.hpp"
+#include "../../../Utils/log/Log.hpp"
 
+SDL_Texture* BotonConTexto::cargoTextura(string texto){
+	Log* log = Log::getInstance();
+	SDL_Color color = { 255, 255, 255, 0xFF };
+	SDL_Texture* texturaACargar;
+	SDL_Surface* textSurface = TTF_RenderText_Solid( this->fuente, texto.c_str(), color );
+	if( textSurface != NULL ){
+		texturaACargar = SDL_CreateTextureFromSurface( this->renderer, textSurface );
+		if( texturaACargar == NULL ){
+			log->huboUnErrorSDL("No se pudo crear la textura", SDL_GetError());
+		}
 
-BotonConTexto::BotonConTexto(int posicionX, int posicionY, int ancho, int alto , SDL_Texture* texto){
-	this->rectangulo = {posicionX, posicionY, ancho , alto };
-	this->texto = texto;
+		SDL_FreeSurface( textSurface );
+	}
+	else{
+		log->huboUnErrorSDL("No se pudo renderizar la superficie", SDL_GetError());
+		log->huboUnErrorSDL(texto, SDL_GetError());
+	}
+	return texturaACargar;
 }
 
-void BotonConTexto::cambiarTexto(SDL_Texture* texto){
+BotonConTexto::BotonConTexto(int posicionX, int posicionY, int ancho, int alto , string texto, SDL_Renderer* renderer, TTF_Font* fuente){
+	Log* log = Log::getInstance();
+	this->rectangulo = {posicionX, posicionY, ancho , alto };
 	this->texto = texto;
+	this->fuente = fuente;
+	this->renderer = renderer;
+	this->texturaTexto = this->cargoTextura(texto);
+
+}
+
+void BotonConTexto::cambiarTexto(string texto){
+	this->texto = texto;
+	SDL_Texture* texturaABorrar = this->texturaTexto;
+	if(!texto.empty()){
+		this->texturaTexto = this->cargoTextura(texto);
+	}else{
+		this->texturaTexto = this->cargoTextura("...");
+	}
+
+	SDL_DestroyTexture( texturaABorrar );
 }
 
 bool BotonConTexto::botonClickeado(SDL_Event evento){
@@ -33,18 +66,18 @@ void renderizar(int coordenadaX,int coordenadaY,int alto,int ancho,SDL_Texture* 
 		SDL_RenderCopyEx( renderer, textura, clip, &renderQuad, angle, center, flip );
 }
 
-void BotonConTexto::mostrarse(SDL_Renderer* renderer){
+void BotonConTexto::mostrarse(){
 
-	renderizar(rectangulo.x + rectangulo.x*0.05,rectangulo.y + rectangulo.y *0.05 ,rectangulo.h * 0.7,rectangulo.w * 0.7,texto,renderer);
+	renderizar(rectangulo.x + rectangulo.x*0.05,rectangulo.y + rectangulo.y *0.05 ,rectangulo.h * 0.7,rectangulo.w * 0.7,texturaTexto,renderer);
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0xFF );
 
 	SDL_RenderDrawRect(renderer, &rectangulo);
 }
 
-void BotonConTexto::mostrarseCambiandoAncho(SDL_Renderer* renderer, int nuevoAncho){
+void BotonConTexto::mostrarseCambiandoAncho(int nuevoAncho){
 
-	renderizar(rectangulo.x ,rectangulo.y,rectangulo.h , nuevoAncho,texto,renderer);
+	renderizar(rectangulo.x ,rectangulo.y,rectangulo.h , nuevoAncho,texturaTexto,renderer);
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0xFF );
 

1aee241563fec041e79350a6c79c49a0a43f5ea8
Joaquin Gomez <joaqogomez@gmail.com>
2021-01-05 17:19:59 -0300

    bug fix musica pantalla inicio

--- src/Client/reproductorDeMusica/EstadoMusica.cpp
+++ src/Client/reproductorDeMusica/EstadoMusica.cpp
@@ -1,20 +1,37 @@
 #include "EstadoMusica.hpp"
 #include "ReproductorMusica.hpp"
 
+Suena::Suena(string cancionAReproducir,string cancionQueSeReproduce){
+    this->cancionAReproducir = cancionAReproducir;
+    this->cancionQueSeReproduce = cancionQueSeReproduce;
+}
+
 EstadoMusica* Suena::cambiar() {
     ReproductorMusica::getInstance()->pausarMusica();
-    return new Muteada();
+    return new Muteada(cancionAReproducir,cancionQueSeReproduce);
 }
 
 void Suena::reproducir(string nombreCancion) {
-    ReproductorMusica::getInstance()->ponerMusica(nombreCancion);
+    if ((cancionQueSeReproduce == cancionAReproducir) && (!cancionQueSeReproduce.empty())){
+        ReproductorMusica::getInstance()->reanudarMusica();
+    }else{
+        ReproductorMusica::getInstance()->ponerMusica(nombreCancion);
+        ReproductorMusica::getInstance()->reanudarMusica();
+        cancionQueSeReproduce = nombreCancion;
+    }
+}
+
+Muteada::Muteada(string cancionAReproducir,string cancionQueSeReproduce){
+    this->cancionAReproducir = cancionAReproducir;
+    this->cancionQueSeReproduce = cancionQueSeReproduce;
 }
 
 EstadoMusica* Muteada::cambiar() {
-    ReproductorMusica::getInstance()->reanudarMusica();
-    return new Suena();
+    EstadoMusica* nuevoEstado = new Suena(cancionAReproducir,cancionQueSeReproduce);
+    nuevoEstado->reproducir(cancionAReproducir);
+    return nuevoEstado;
 }
 
 void Muteada::reproducir(string nombreCancion) {
-    // No se reproduce nada
+    cancionAReproducir = nombreCancion;
 }
--- src/Client/reproductorDeMusica/EstadoMusica.hpp
+++ src/Client/reproductorDeMusica/EstadoMusica.hpp
@@ -6,20 +6,26 @@ using namespace std;
 
 class EstadoMusica {
     public:
-        virtual ~EstadoMusica()= default;;
+        virtual ~EstadoMusica()= default;
         virtual EstadoMusica* cambiar() = 0;
         virtual void reproducir(string nombreCancion) = 0;
+
+    protected:
+        string cancionAReproducir;
+        string cancionQueSeReproduce;
 };
 
 class Suena : public EstadoMusica{
     public:
+        Suena()= default;
+        Suena(string cancionAReproducir,string cancionQueSeReproduce);
         EstadoMusica* cambiar() override;
         void reproducir(string nombreCancion) override;
-
 };
 
 class Muteada : public EstadoMusica{
     public:
+        Muteada(string cancionAReproducir,string cancionQueSeReproduce);
         EstadoMusica* cambiar() override;
         void reproducir(string nombreCancion) override;
 };

81fc00a5f281c67db771dd836b543de372915648
Bruno Grassano <grassanobruno@gmail.com>
2021-01-27 13:03:19 -0300

    ref: Se eliminan conexiones perdidas durante el transcurso del server
    -m "Antes lo haciamos al terminar el servidor solamente"

---
+++ src/Server/Servidor/IntentadorIniciarModelo.hpp
@@ -0,0 +1,28 @@
+#ifndef TP_TALLER_DE_PROGRAMACION_FIUBA_INTENTADORINICIARMODELO_H
+#define TP_TALLER_DE_PROGRAMACION_FIUBA_INTENTADORINICIARMODELO_H
+
+#include <map>
+#include <string>
+
+#include "src/Server/Modelo/AplicacionServidor.hpp"
+#include "src/Utils/Thread.hpp"
+#include "ConexionCliente.hpp"
+
+
+class IntentadorIniciarModelo : public Thread{
+
+    public:
+        IntentadorIniciarModelo(unsigned int cantidadConexiones, AplicacionServidor* aplicacionServidor,
+                                std::map<int,ConexionCliente*>* mapaClientesJugando,
+                                std::map<int,std::string> *mapaIDNombre);
+        void ejecutar() override;
+
+    private:
+        AplicacionServidor* aplicacionServidor{};
+        std::map<int,ConexionCliente*>* mapaClientesJugando;
+        std::map<int,std::string> *mapaIDNombre;
+        const unsigned int cantidadConexiones;
+};
+
+
+#endif //TP_TALLER_DE_PROGRAMACION_FIUBA_INTENTADORINICIARMODELO_H
